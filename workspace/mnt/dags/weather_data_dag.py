d1a5e95ea3f236ec59075dcbc4ee7732c51670c9
krittapatph-web <krittapat.ph@gmail.com>
2026-06-21 07:43:16 +0000

    Try analytics engineering with duckdb

--- workspace/mnt/dags/weather_data_dag.py
+++ workspace/mnt/dags/weather_data_dag.py
@@ -25,6 +25,40 @@ def _get_air_Quality_data(**context):
     with open(f"/opt/airflow/dags/{ds}-output.json", "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
+def _check_pm2_5_key_exists(**context):
+    pass
+    ds = context["ds"]
+    
+    import json
+
+    with open(f"/opt/airflow/dags/{ds}-output.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    assert "hourly" in data ,"Key 'hourly' not found in the data"
+    assert "pm2_5" in data["hourly"] , "Key 'pm2_5' not found in the 'hourly' data"
+    
+
+def _calcuate_data_quality_score(**context):
+    import json
+    import glob
+    import os
+
+    pattern = os.path.join("/opt/airflow/dags", "*-output.json")
+    files = sorted(glob.glob(pattern))
+    results = []
+    count_ok = 0
+    for f in files:
+        with open(f, "r", encoding="utf-8") as fh:
+            data = json.load(fh)
+        pm = data["hourly"]["pm2_5"]
+        pm_count = len(pm)
+        if pm_count == 24:
+            count_ok += 1
+
+    total_files = len(files)
+    score = (count_ok / total_files)
+    print(f"Data Quality Score: {score}")
+
 
 def _find_average_pm2_5(**context):
     ds = context["ds"]
@@ -44,6 +78,47 @@ def _find_average_pm2_5(**context):
         }
         json.dump(data, f, ensure_ascii=False, indent=2) 
 
+def _transform_to_csv(**context):
+    import csv
+    import json
+    import glob
+    import os
+
+    pattern = os.path.join("/opt/airflow/dags", "*-output.json")
+    files = sorted(glob.glob(pattern))
+    results = []
+    count_ok = 0
+    with open("/opt/airflow/dags/pm2_5_data.csv", "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["date", "hour", "pm2_5"])
+        for f in files:
+            with open(f, "r", encoding="utf-8") as fh:
+                data = json.load(fh)        
+                for i, pm_value in enumerate(data["hourly"]["pm2_5"]):
+                    writer.writerow([data["hourly"]["time"][0].split("T")[0], i, pm_value])
+
+
+def _find_average_pm2_5_in_duckdb(**context):
+    import duckdb
+    import os
+
+    conn = duckdb.connect(database="/opt/airflow/dags/myduckdb.db")
+    csv_file_path = "/opt/airflow/dags/pm2_5_data.csv"
+    conn.execute(f"""
+        CREATE TABLE IF NOT EXISTS pm2_5_data AS 
+        SELECT * FROM read_csv_auto('{csv_file_path}')
+    """)
+
+    query = """
+        CREATE OR REPLACE VIEW avg_pm2_5 as
+        select
+            date,
+            avg(pm2_5)
+        from pm2_5_data
+        group by date
+    """
+    conn.execute(query)
+
 
 with DAG(
     "weather_data_dag",
@@ -54,15 +129,39 @@ with DAG(
         task_id="weather_data_dag",
         python_callable=_get_air_Quality_data,
     )
+    
+    check_pm2_5_key_exists = PythonOperator(
+    task_id="check_pm2_5_key_exists",
+    python_callable=_check_pm2_5_key_exists,
+    )  
+
+    calculate_data_quality_score = PythonOperator(
+        task_id="calculate_data_quality_score",
+        python_callable=_calcuate_data_quality_score,
+    )
 
     find_average_pm2_5 = PythonOperator(
         task_id="find_average_pm2_5",
         python_callable=_find_average_pm2_5,
     )  
 
-    get_air_Quality_data >> find_average_pm2_5
+    transform_to_csv = PythonOperator(
+        task_id="transform_to_csv",
+        python_callable=_transform_to_csv,
+    )  
 
-    echo_date = BashOperator(
-        task_id="echo_date",
-        bash_command="echo {{ ds }}",
-    )
+    find_average_pm2_5_in_duckdb = PythonOperator(
+        task_id="find_average_pm2_5_in_duckdb",
+        python_callable=_find_average_pm2_5_in_duckdb,
+    )  
+
+    get_air_Quality_data >> check_pm2_5_key_exists >> find_average_pm2_5
+    check_pm2_5_key_exists >> calculate_data_quality_score 
+    check_pm2_5_key_exists >> transform_to_csv >> find_average_pm2_5_in_duckdb
+
+
+    # echo_date = BashOperator(
+    #     task_id="echo_date",
+    #     bash_command="echo {{ ds }}",
+    # )
+
